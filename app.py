33b3f886f09de17d8c8c547ecebcf32a7dfa7d07
Francesco <francesco98m@gmail.com>
2022-05-28 11:59:59 +0200

    adde yfinance

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+from pandas import Period
 import streamlit as st
+import yfinance as yf
 
-st.title("Squadra è stupido")
+st.title("A Squadra piacciono i soldi")
+
+msft = yf.Ticker("MSFT")
+
+hist_data = msft.history(period='5y')
+
+st.write(hist_data)
