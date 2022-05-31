cf976d9e76954bf1e07bff5a0403227849dd0a29
Francesco <francesco98m@gmail.com>
2022-05-31 14:22:47 +0200

    twittosity graph done, check dates

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 import yfinance as yf
 import twint as tw
 import datetime as dt
+import matplotlib.pyplot as plt
 
 
 def get_tweets(date, keyword, likes):
@@ -12,8 +13,8 @@ def get_tweets(date, keyword, likes):
     c.Hide_output = True
     c.Search = keyword
     c.Min_likes = likes
-    c.Since = str(date)+" 00:00:00"
-    c.Until = str(date)+" 23:59:59"
+    c.Since = str(date)
+    c.Until = str(date+dt.timedelta(hours=23, minutes=59, seconds=59))
     c.Pandas = True
     tw.run.Search(c)
 
@@ -23,37 +24,44 @@ def get_tweets(date, keyword, likes):
 st.title("A Tutti piacciono i soldi")
 st.header("Twittosity")
 
+
+
 with st.form("Paramaters"):
-    date = st.date_input("Date")
-    keyword = st.text_input("Keyword")
+    start = st.date_input("Enter start date", dt.date(2022, 1, 1))
+    end = st.date_input("Enter end date", dt.datetime.now().date())
+    keyword = "bitcoin"
     likes = st.slider("Likes",100,2000,500,10)
     submitted = st.form_submit_button("Search")
 
 if submitted:
-    with st.spinner("Wait for it..."):
-        get_tweets(date, keyword, likes)
+    tmp = []
+    dates_window = pd.date_range(start=start, end=end)
+    bar = st.progress(0)
+    tot_len = len(dates_window)
+    for i, day in enumerate(dates_window):
+        get_tweets(day, keyword, likes)
+        bar.progress((i+1)/tot_len)
+        tweets_df = tw.storage.panda.Tweets_df
+        if tweets_df.empty:
+            tmp.append([day.date(),0])
+        else:
+            tmp.append([day.date(),len(tweets_df['tweet'])])
+        
+    twittosity = pd.DataFrame(tmp, columns=['date','n_tweet'])
+
+    value = yf.download('BTC-EUR', start, end)
+
+    value['Mean'] = value[['High', 'Low']].mean(axis=1)
     
-    tweets_df = tw.storage.panda.Tweets_df
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    fig, ax1 = plt.subplots()
+    ax1.plot(value['Mean'], color='red')
+    ax1.xaxis.set_major_locator(plt.MaxNLocator(5))
+    ax2 = ax1.twinx()
+    ax2.plot(twittosity['date'], twittosity['n_tweet'], color='blue')
+    ax2.xaxis.set_major_locator(plt.MaxNLocator(5))
+    fig.tight_layout()
     
-    #cleaned = tweets_df[['tweet','username','nlikes']].sort_values(['nlikes'], ascending=False)
 
-    st.table(tweets_df)
-
-
-
-
-#desired_money = st.slider("Quanti soldi vuoi?", 2, 5002, 2000)
-
-
-#msft = yf.Ticker("MSFT")
-
-#hist_data = msft.history(period='5y')
-
-start = st.date_input("Enter start date", dt.date(2022, 1, 1))
-end = st.date_input("Enter end date", dt.datetime.now().date())
-
-value = yf.download('BTC', start, end)
-
-value['Mean'] = value[['High', 'Low']].mean(axis=1)
-st.write(value)
-st.line_chart(data=value['Mean'], width=0, height=0, use_container_width=True)
+    st.pyplot(fig)
